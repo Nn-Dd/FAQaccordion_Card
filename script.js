1b35d65c3d8d7ed1d889bf5318f8ee1edff53ccf
NnDd <imenez.andy05@gmail.com>
2023-09-26 10:50:12 -0600

    Added the corresponding code

---
+++ script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Select all elements with the class "questions"
+  var coll = document.querySelectorAll(".questions");
+
+  // Add click event listeners to each "questions" element
+  coll.forEach(function (question) {
+    var title = question.querySelector(".title");
+    var paragraph = question.querySelector(".paragraph");
+    var icon = title.querySelector(".icon");
+    var heading = title.querySelector(".question-heading");
+
+    title.addEventListener("click", function () {
+      // Check if the clicked question is active
+      var isActive = question.classList.contains("active");
+
+      // Close all open questions
+      coll.forEach(function (otherQuestion) {
+        otherQuestion.classList.remove("active");
+        otherQuestion.querySelector(".paragraph").style.display = "none";
+        otherQuestion.querySelector(".icon").style.transform = `rotate(0deg)`;
+        otherQuestion.querySelector(
+          ".question-heading"
+        ).style.color = `hsl(240, 6%, 50%)`;
+        otherQuestion.querySelector(
+          ".question-heading"
+        ).style.fontWeight = `400`;
+      });
+
+      // If the clicked question wasn't active, open it
+      if (!isActive) {
+        question.style.transition = "all 2s";
+        question.classList.add("active");
+        paragraph.style.display = "block";
+        icon.style.transform = `rotate(180deg)`;
+        heading.style.color = `#201e36`;
+        heading.style.fontWeight = `700`;
+      }
+    });
+  });
+});
